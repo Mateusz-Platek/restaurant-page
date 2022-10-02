77230d2cbae161ba53dac1d5c8a6929a7985f307
Mateusz-Platek <m.platek1705@gmail.com>
2022-10-02 19:58:59 +0200

    Rework home page

--- src/myHome.js
+++ src/myHome.js
@@ -4,6 +4,13 @@ export default function compHome() {
     let main = document.createElement('div');
     main.classList.add('main-home');
 
+    let imgbox = document.createElement('div');
+    imgbox.classList.add('homeimg');
+    let homeImg = document.createElement('img');
+    homeImg.src = pizzaPhoto;
+    imgbox.appendChild(homeImg);
+    main.appendChild(imgbox);
+
     let title = document.createElement('h2');
     title.textContent = "Welcome at Matthew's Pizza!"
     main.appendChild(title);
@@ -11,31 +18,58 @@ export default function compHome() {
     let grid = document.createElement('div');
     grid.classList.add('grid');
 
-    let imgbox = document.createElement('div');
-    imgbox.classList.add('homeimg');
-    let homeImg = document.createElement('img');
-    homeImg.src = pizzaPhoto;
-    imgbox.appendChild(homeImg);
-    grid.appendChild(imgbox);
+    let leftText = document.createElement('div');
+    leftText.classList.add('left');
+    leftText.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci eu lobortis elementum nibh tellus molestie nunc non blandit.';
+    grid.appendChild(leftText);
 
-    let sidetext = document.createElement('div');
-    sidetext.classList.add('sidetext');
+    let rightText = document.createElement('div');
+    rightText.classList.add('right');
 
-    let header = document.createElement('h2');
-    header.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci eu lobortis elementum nibh tellus molestie nunc non blandit.';
-    sidetext.appendChild(header);
-
-    let para1 = document.createElement('p');
+    let para1 = document.createElement('div');
     para1.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
-    sidetext.appendChild(para1);
+    rightText.appendChild(para1);
 
-    let para2 = document.createElement('p');
+    let para2 = document.createElement('div');
     para2.textContent = 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-    sidetext.appendChild(para2);
+    rightText.appendChild(para2);
 
-    grid.appendChild(sidetext);
+    grid.appendChild(rightText);
 
     main.appendChild(grid);
+
+    // let title = document.createElement('h2');
+    // title.textContent = "Welcome at Matthew's Pizza!"
+    // main.appendChild(title);
+
+    // let grid = document.createElement('div');
+    // grid.classList.add('grid');
+
+    // let imgbox = document.createElement('div');
+    // imgbox.classList.add('homeimg');
+    // let homeImg = document.createElement('img');
+    // homeImg.src = pizzaPhoto;
+    // imgbox.appendChild(homeImg);
+    // grid.appendChild(imgbox);
+
+    // let sidetext = document.createElement('div');
+    // sidetext.classList.add('sidetext');
+
+    // let header = document.createElement('h2');
+    // header.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci eu lobortis elementum nibh tellus molestie nunc non blandit.';
+    // sidetext.appendChild(header);
+
+    // let para1 = document.createElement('p');
+    // para1.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+    // sidetext.appendChild(para1);
+
+    // let para2 = document.createElement('p');
+    // para2.textContent = 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    // sidetext.appendChild(para2);
+
+    // grid.appendChild(sidetext);
+
+    // main.appendChild(grid);
 
     return main;
 }
